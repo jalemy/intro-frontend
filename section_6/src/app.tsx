a31add7f4cc8f8f2da2a74e24e70239345b3b19d
jalemy <jalemy.xps@gmail.com>
2021-01-22 20:07:08 +0900

    fix: webpack dev server

--- section_6/src/app.tsx
+++ section_6/src/app.tsx
@@ -2,5 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const root = document.getElementById('react-root')
-ReactDOM.render(<h1>Reactほげほげfugafugaで描画する</h1>, root)
-
+ReactDOM.render(<h1>Reactで描画する</h1>, root)
--- section_6/webpack.config.js
+++ section_6/webpack.config.js
@@ -12,9 +12,7 @@ module.exports = {
   },
   devServer: {
     contentBase: __dirname,
-    liveReload: true,
-    hot: true,
-    watchContentBase: true,
+    publicPath: '/dist/',
     port: 5000
   },
   module: {
